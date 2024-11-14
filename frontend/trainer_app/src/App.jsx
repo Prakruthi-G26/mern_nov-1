6378b6d526727b6e4eb86e4661b5e0d82eca3561
Prakruthi-G-pakku <2022is_prakruthig_c@nie.ac.in>
2024-11-14 16:26:57 +0530

    all

--- frontend/trainer_app/src/App.jsx
+++ frontend/trainer_app/src/App.jsx
@@ -2,7 +2,15 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import { useEffect } from 'react'
+import { Router } from 'react-router-dom'
+import { Routes } from 'react-router-dom'
+import { Link } from 'react-router-dom'
+import { Route } from 'react-router-dom'
+import { all } from 'axios'
+import GetTrainer from './component/getTrainer'
+import AddTrainer from './component/addTrainer'
+import EditTrainer from './component/editTrainer'
 
 function App() {
   // const [count, setCount] = useState(0)
